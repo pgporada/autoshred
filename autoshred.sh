da6fa0790c6e87e715e4c7463038df53bd7fa96b
pgporada <philporada@gmail.com>
2015-09-22 13:30:44 +0000

    Separated some logic into functions. Added a shredder and root check function. Improved the usage function

--- autoshred.sh
+++ autoshred.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # AUTHOR:   Phil Porada - philporada@gmail.com
-# WHAT:     Automatically runs nwipe on any media plugged into the computer aside from devices in the exclusion list
+# WHAT:     Automatically runs nwipe dodshort (3 passes) on any media plugged into the computer aside from devices in the exclusion list
 # WHY:      because automagic
 
-
 EXCLUSION=("sda" "sdb" "sr")
+
 BLD=$(tput bold)
 RST=$(tput sgr0)
 RED=$(tput setaf 1)
@@ -13,68 +13,35 @@ BLU=$(tput setaf 4)
 KEYPRESS=""
 
 usage() {
-    echo "${BLD}${RED}WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST${RST}"
-    echo "${BLD}${RED}WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST${RST}"
-    echo "${BLD}${RED}WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST${RST}"
-    echo "${BLD}${RED}WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST${RST}"
-    echo "${BLD}${RED}WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST${RST}"
+    echo "${BLD}${RST}#####################################################################################${RST}"
+    echo "${BLD}${RED}# WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST #${RST}"
+    echo "${BLD}${RED}# WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST #${RST}"
+    echo "${BLD}${RED}# WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST #${RST}"
+    echo "${BLD}${RED}# WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST #${RST}"
+    echo "${BLD}${RED}# WARNING: THIS SCRIPT WILL NUKE DATA IN ANY BLOCK DEVICE NOT IN THE EXCLUSION LIST #${RST}"
+    echo "${BLD}${RST}#####################################################################################${RST}"
     echo
-    echo "${BLD}Current exclusion list${RST}"
+    echo "${BLD}Current exclusion list. THIS IS IMPORTANT.${RST}"
     echo "${BLD}+--------------------+${RST}"
     for i in ${EXCLUSION[@]}; do
         echo "/dev/$i"
     done
     echo
-    echo "${BLD}Example usage${RST}"
-    echo "${BLD}+-----------+${RST}"
-    echo "This will run the script and start wiping your data with a DoD 3 pass"
-    echo "sudo ./$(basename $0) -f"
+    echo "${BLD}Usage${RST}"
+    echo "${BLD}+--------------------+${RST}"
+    echo "[ -f ]   |   Run the script. By default this will be 3 passes of the DoD wipe."
+    echo "Example: sudo ./$(basename $0) -f"
+    echo
+    echo "[ -h ]   |   Show this help message."
+    echo "Example: ./$(basename $0) -h"
+    echo
+    echo "[ -s ]   |   Display Shredder"
+    echo "Example: ./$(basename $0) -s"
+    echo
+    echo "${BLD}Important Read for Data Sanitization${RST}"
+    echo "${BLD}+---------------------+${RST}"
+    echo "http://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-88r1.pdf"
 }
-
-# Run only with root privs due to the forceful unmounting we need to do.
-# You can't sudo echo. You can technically... but whatever
-if [ $EUID -ne 0 ]; then
-   usage
-   exit 1
-fi
-
-if [ $# -ne 1 ]; then
-    usage
-    exit 1
-fi
-
-while getopts ":f" opt; do
-  case $opt in
-    f)
-      echo "${BLD}Running!${RST}" >&2
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2;
-      usage;
-      exit 1;
-      ;;
-  esac
-done
-
-
-if [ -f /etc/redhat-release ] ; then
-    rpm -q nwipe &>/dev/null
-    if [ $? -eq 1 ]; then
-        echo "${BLD}${RED}===> Nwipe not found. Installing nwipe${RST}"
-        sudo yum install -y nwipe 
-        echo "${BLD}${GRN}===> Nwipe installed @ $(which nwipe)${RST}"
-    fi
-elif [ -f /etc/debian_version ]; then
-    dpkg-query -l nwipe &>/dev/null
-    if [ $? -eq 1 ]; then
-        echo "${BLD}${RED}===> Nwipe not found. Installing nwipe${RST}"
-        sudo apt-get install -y nwipe 
-        echo "${BLD}${GRN}===> Nwipe installed @ $(which nwipe)${RST}"
-    fi
-else 
-    echo "${BLD}${RED}===> Unsupported disto at this time${RST}"
-    exit 1
-fi
 
 # Thanks to http://www.retrojunkie.com/asciiart/cartchar/turtles.htm
 shredder_ascii() {
@@ -125,6 +92,68 @@ display_header() {
     echo "                               ${BLD}${BLU}#${RST}${BLD} ==> because fuck your data <== ${BLU}#${RST}"
     echo "                               ${BLD}${BLU}##################################${RST}"
 }
+
+root_check() {
+    # Run only with root privs due to the forceful unmounting we need to do.
+    # You can't sudo echo. You can technically... but whatever
+    if [ $EUID -ne 0 ]; then
+       usage
+       exit 1
+    fi
+}
+
+script_update() {
+    DIR=$(dirname "$(readlink -f "$0")")
+    cd "${DIR}"
+    git pull
+}
+
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+while getopts ":f:h:s" opt; do
+  case $opt in
+    f)
+      root_check;
+      echo "${BLD}Running!${RST}" >&2;
+      ;;
+    h)
+      usage;
+      exit 2;
+      ;;
+    s) 
+      shredder_ascii;
+      exit 2;
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2;
+      usage;
+      exit 1;
+      ;;
+  esac
+done
+
+
+if [ -f /etc/redhat-release ] ; then
+    rpm -q nwipe &>/dev/null
+    if [ $? -eq 1 ]; then
+        echo "${BLD}${RED}===> Nwipe  not found. Installing nwipe${RST}"
+        sudo yum install -y nwipe
+        echo "${BLD}${GRN}===> Nwipe installed @ $(which nwipe)${RST}"
+    fi
+elif [ -f /etc/debian_version ]; then
+    dpkg-query -l nwipe &>/dev/null
+    if [ $? -eq 1 ]; then
+        echo "${BLD}${RED}===> Nwipe not found. Installing nwipe${RST}"
+        sudo apt-get install -y nwipe
+        echo "${BLD}${GRN}===> Nwipe installed @ $(which nwipe)${RST}"
+    fi
+else 
+    echo "${BLD}${RED}===> Unsupported disto at this time${RST}"
+    exit 1
+fi
 
 
 clear

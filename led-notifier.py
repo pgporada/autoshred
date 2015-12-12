563c31a139fe6063971ab2dc8dc0019558ae1c3a
Phil Porada <philporada@gmail.com>
2015-12-12 21:16:47 +0000

    Added a basic python script that blinks an LED on a Raspberry Pi breadboard

---
+++ led-notifier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# AUTHOR: Phil Porada - philporada@gmail.com
+
+from time import sleep
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("You need to run as sudo")
+
+LED=19
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+
+GPIO.output(LED, GPIO.HIGH)
+sleep(.5)
+GPIO.output(LED, GPIO.LOW)
+
+GPIO.cleanup()
